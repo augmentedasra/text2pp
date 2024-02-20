fe3b9dc22a5951658b5afc6085a6b291ca1529ed
Asra <159510414+augmentedasra@users.noreply.github.com>
2024-02-20 23:48:42 +0330

    Update texttwopp.py

--- texttwopp.py
+++ texttwopp.py
@@ -1,7 +1,7 @@
 import os
 from gtts import gTTS
 myText=input("write some bs: ")
-language="en"
+language="hi"
 myObj=gTTS(text=myText, lang=language, slow=False)
 myObj.save("shit.mp3")
 os.system("mpg123 shit.mp3")
